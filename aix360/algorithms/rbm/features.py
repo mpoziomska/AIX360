69928d300985a1e9d26af943e2fa6bc7a4cb0eb2
mpoziomska <58225940+mpoziomska@users.noreply.github.com>
2021-03-10 17:07:30 +0100

    Update features.py

--- aix360/algorithms/rbm/features.py
+++ aix360/algorithms/rbm/features.py
@@ -25,17 +25,12 @@ class FeatureBinarizer(TransformerMixin):
             returnOrd (bool): Also return standardized ordinal features
         """
         # List of categorical columns
-        print(colCateg)
         if type(colCateg) is pd.Series:
             self.colCateg = colCateg.tolist()
-            print("A")
         elif type(colCateg) is not list:
-            print("B")
             self.colCateg = [colCateg]
         else:
-            print("C")
             self.colCateg = colCateg
-        print(self.colCateg)
         # Number of quantile thresholds used to binarize ordinal features
         self.numThresh = numThresh
         self.thresh = {}
@@ -75,7 +70,6 @@ class FeatureBinarizer(TransformerMixin):
         for c in data:
             # number of unique values
             valUniq = data[c].nunique()
-            print(c in self.colCateg)
             # Constant or binary column
             if valUniq <= 2:
                 # Mapping to 0, 1
