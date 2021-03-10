9c7f00550ae7e392846b876aca987539b8834106
mpoziomska <58225940+mpoziomska@users.noreply.github.com>
2021-03-10 17:48:05 +0100

    Update features.py

--- aix360/algorithms/rbm/features.py
+++ aix360/algorithms/rbm/features.py
@@ -79,7 +79,7 @@ class FeatureBinarizer(TransformerMixin):
                 # OneHotEncoder object
                 enc[c] = OneHotEncoder(sparse=False, dtype=int, handle_unknown='ignore')
                 # Fit to observed categories
-                #print(data[[c]], "\nA\n")
+                print(type(data[[c]]), type(data[c]), data[c])
                 enc[c].fit(data[[c]])
 
             # Ordinal column
